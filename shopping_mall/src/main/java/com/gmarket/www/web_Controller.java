8b08959901cd9bc7587bafba666490300750db7f
chgyeol <ch.gyeol95@gmail.com>
2024-07-26 11:47:33 +0900

    web_controller

--- shopping_mall/src/main/java/com/gmarket/www/web_Controller.java
+++ shopping_mall/src/main/java/com/gmarket/www/web_Controller.java
@@ -3,12 +3,15 @@ package com.gmarket.www;
 import java.io.PrintWriter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class web_Controller {
 	
 	PrintWriter pw = null;
-	
-	
+	@GetMapping("ajaxok.do")
+	public String ajaxok() {
+		return null;
+	}
 	
 }
